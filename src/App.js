0ca6d6242f49b2c6c412aeac063c1c96155529d5
ding-swe <eric7055@gmail.com>
2022-07-13 01:37:51 -0400

    introduce react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> Hello world, I'm <b>Eric Ding</b>! </p>
+        <p> Check out my <a href="https://www.linkedin.com/in/ebding/">LinkedIn</a>! </p>
+        <p> If you'd like to read some of my longer thoughts, consider <a href="https://medium.com/@kaleandchicken">my Medium writings</a> </p>
+        <p> Lastly, I dabble in some filmmaking. You can find my short film on <a href="https://www.youtube.com/channel/UCo5aawBt6RBmeEJ3uBdSm3A">YouTube</a> </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
